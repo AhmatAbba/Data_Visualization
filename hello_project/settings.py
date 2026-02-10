f723df3c44a3cd77a1aaed495800ab173c4cf838
AhmatAbba <mahamatabbaahmat4@gmail.com>
2026-02-10 17:42:48 +0200

    Auto-fix unescaped characters in DATABASE_URL

--- hello_project/settings.py
+++ hello_project/settings.py
@@ -78,11 +78,34 @@ WSGI_APPLICATION = 'hello_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+tmp_db_url = os.environ.get('DATABASE_URL')
+if tmp_db_url:
+    # Auto-fix unescaped # in password (common issue)
+    if '#' in tmp_db_url and 'postgres' in tmp_db_url:
+        # Assuming format postgresql://user:password@host...
+        try:
+            scheme, rest = tmp_db_url.split('://', 1)
+            creds, location = rest.split('@', 1)
+            user, password = creds.split(':', 1)
+            if '#' in password:
+                password = password.replace('#', '%23')
+                tmp_db_url = f"{scheme}://{user}:{password}@{location}"
+        except ValueError:
+            pass  # Fallback to original if parsing fails
+
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
+    'default': dj_database_url.parse(
+        tmp_db_url,
+        conn_max_age=600,
+        conn_health_checks=True,
     )
+} if tmp_db_url else {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
